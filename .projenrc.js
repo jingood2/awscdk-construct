5cd9afb28f14a1797f77b2732a30b54082b03a72
jingood2 <jingood2@gmail.com>
2021-11-26 16:15:12 +0900

    feat: release 0.0.1

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,25 @@
 const { AwsCdkConstructLibrary } = require('projen');
+
+const PROJECT_NAME = 'cdk-sample-lib' ;
+
 const project = new AwsCdkConstructLibrary({
   author: 'jingood2',
   authorAddress: 'jingood2@gmail.com',
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.134.0',
   defaultReleaseBranch: 'main',
   name: 'awscdk-construct',
   repositoryUrl: 'https://github.com/jingood2/awscdk-construct.git',
 
-  // cdkDependencies: undefined,      /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-apigatewayv2',
+    '@aws-cdk/aws-apigatewayv2-integrations',
+    '@aws-cdk/aws-lambda',
+  ], /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  python: {
+    distName: PROJECT_NAME,
+    module: 'cdk_sample_lib',
+  },
   // cdkTestDependencies: undefined,  /* AWS CDK modules required for testing. */
   // deps: [],                        /* Runtime dependencies of this module. */
   // description: undefined,          /* The description is just a string that helps people understand the purpose of the package. */
---
+++ API.md
@@ -0,0 +1,33 @@
+# API Reference <a name="API Reference"></a>
+
+## Constructs <a name="Constructs"></a>
+
+### CdkSampleLib <a name="awscdk-construct.CdkSampleLib"></a>
+
+#### Initializers <a name="awscdk-construct.CdkSampleLib.Initializer"></a>
+
+```typescript
+import { CdkSampleLib } from 'awscdk-construct'
+
+new CdkSampleLib(scope: Construct, id: string)
+```
+
+##### `scope`<sup>Required</sup> <a name="awscdk-construct.CdkSampleLib.parameter.scope"></a>
+
+- *Type:* [`@aws-cdk/core.Construct`](#@aws-cdk/core.Construct)
+
+---
+
+##### `id`<sup>Required</sup> <a name="awscdk-construct.CdkSampleLib.parameter.id"></a>
+
+- *Type:* `string`
+
+---
+
+
+
+
+
+
+
+
